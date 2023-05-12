da47ea92b43d04dc9fe7babbbc7d368452c8f56c
Kevin Stamps <kevin.m.stamps@gmail.com>
2023-05-12 11:42:37 -0500

    Çhanges made to the project ~May 10.

--- MVScheckin.py
+++ MVScheckin.py
@@ -9,12 +9,14 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form['name']
+    date = request.form['date']
+    last_name = request.form['last_name']
+    first_name = request.form['first_name']
     address = request.form['address']
     zipcode = request.form['zipcode']
     phone = request.form['phone']
     email = request.form['email']
-    branch = request.form['branch']
+    which_applies_to_you = request.form['which_applies_to_you']
     relationship = request.form['relationship']
     reason = request.form['reason']
 
@@ -23,15 +25,17 @@ def submit():
     if reason not in reason_options:
         return '<h1>Please select a valid reason for visit.</h1>'
     if reason == 'Other':
-        return render_template('other_reason.html', name=name, address=address, zipcode=zipcode, phone=phone, email=email, branch=branch, relationship=relationship)
+        return render_template('other_reason.html', first_name=first_name, last_name=last_name, address=address, zipcode=zipcode, phone=phone, email=email, which_applies_to_you=which_applies_to_you, relationship=relationship)
+
+    if not first_name or not last_name:
+        return '<h1>Please enter a first and last name.</h1>'
 
     with open('visitors.csv', 'a', newline='') as csvfile:
-        fieldnames = ['name', 'address', 'zipcode', 'phone', 'email', 'branch', 'relationship', 'reason']
+        fieldnames = ['first_name', 'last_name', 'address', 'zipcode', 'phone', 'email', 'which_applies_to_you', 'relationship', 'reason']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerow({'name': name, 'address': address, 'zipcode': zipcode, 'phone': phone, 'email': email, 'branch': branch, 'relationship': relationship, 'reason': reason})
+        writer.writerow({'first_name': first_name, 'last_name': last_name, 'address': address, 'zipcode': zipcode, 'phone': phone, 'email': email, 'which_applies_to_you': which_applies_to_you, 'relationship': relationship, 'reason': reason})
 
     return render_template('thankyou.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
-
+    app.run(debug=True, port=5006)
